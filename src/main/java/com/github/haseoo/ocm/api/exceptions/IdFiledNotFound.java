d2aac63afd907f1618788231238cb688e392f37d
Dawid Szymkiewicz <dawid_szymkiewicz@hotmail.com>
2021-04-29 16:53:14 +0200

    Change id exception message

--- src/main/java/com/github/haseoo/ocm/api/exceptions/IdFiledNotFound.java
+++ src/main/java/com/github/haseoo/ocm/api/exceptions/IdFiledNotFound.java
@@ -2,6 +2,6 @@ package com.github.haseoo.ocm.api.exceptions;
 
 public class IdFiledNotFound extends CsvMappingException {
     public IdFiledNotFound(Class<?> clazz) {
-        super(String.format("Class %s has not an id field", clazz.getCanonicalName()));
+        super(String.format("Class %s has no field marked as id", clazz.getCanonicalName()));
     }
 }
